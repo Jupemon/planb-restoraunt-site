e8300ae9f85ebaf0b741fc8e73193368e6267da1
Juho <s6heju04@gmail.com>
2020-01-31 14:15:22 +0200

    Updates

---
+++ precache-manifest.b401e912b49af8a0d709030a004a5e99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c2f0d7f0e77e0528d4b2bee772a7b8",
+    "url": "/planb-restoraunt-site/index.html"
+  },
+  {
+    "revision": "76a9ef5f30f631f22e8a",
+    "url": "/planb-restoraunt-site/static/css/main.69b80e93.chunk.css"
+  },
+  {
+    "revision": "fd4287a0d73d2c9dd2cd",
+    "url": "/planb-restoraunt-site/static/js/2.76d74307.chunk.js"
+  },
+  {
+    "revision": "76a9ef5f30f631f22e8a",
+    "url": "/planb-restoraunt-site/static/js/main.01584f01.chunk.js"
+  },
+  {
+    "revision": "439c6e0f9843ca71cc96",
+    "url": "/planb-restoraunt-site/static/js/runtime~main.a8745c79.js"
+  },
+  {
+    "revision": "980adae03d3e2679aef5110c973f8373",
+    "url": "/planb-restoraunt-site/static/media/image1.980adae0.png"
+  },
+  {
+    "revision": "307376993e4f57e3598ee732d45c165d",
+    "url": "/planb-restoraunt-site/static/media/image2.30737699.png"
+  },
+  {
+    "revision": "2be30d85be42da17bfae9219dcb299a2",
+    "url": "/planb-restoraunt-site/static/media/image3.2be30d85.png"
+  },
+  {
+    "revision": "4ba2d8c6e73b95effec416dc8924728b",
+    "url": "/planb-restoraunt-site/static/media/pizza.4ba2d8c6.png"
+  }
+]);
